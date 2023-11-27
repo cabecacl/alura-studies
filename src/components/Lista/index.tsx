e9c8707527ae50f2efeec2a4bcb87ed652cef596
Cleiton de Aguiar Silva <cleiton.silva@compasso.com.br>
2023-11-27 15:05:42 -0300

    Criando o projeto no git e atualizando

---
+++ src/components/Lista/index.tsx
@@ -0,0 +1,33 @@
+import style from "./Lista.module.scss";
+import Item from './Item';
+import { ITarefa } from "../../types/tarefas";
+
+interface Props {
+    tarefas: ITarefa[],
+    selecionaTarefa: (tarefaSelecionada: ITarefa) => void
+}
+
+function Lista({tarefas, selecionaTarefa}: Props) {
+
+    return (
+        <aside className={style.listaTarefas}>
+            <h2>Estudos do dia!</h2>
+            <ul>
+                {
+                    tarefas.map((item) => (
+                        <Item 
+                            selecionaTarefa={selecionaTarefa}
+                            key={item.id} 
+                            // tarefa={item.tarefa} 
+                            // tempo={item.tempo} 
+                            {...item}
+                        />
+                    ))
+                }
+            </ul>
+
+        </aside>
+    );
+}
+
+export default Lista;
